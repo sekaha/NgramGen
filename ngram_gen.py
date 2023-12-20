92136ac9e27bb3f08cdaeaeff187276a794dc55a
Hakase <davidsommerfield.zg@gmail.com>
2023-12-20 02:04:16 -0600

    made this work as an actual command line script

--- ngram_gen.py
+++ ngram_gen.py
@@ -5,26 +5,31 @@ from collections import Counter
 def get_grams(
     corpus,
     valid_chars=None,
-    remove_chars="",
+    removed="",
     ordered=False,
+    remove_duplicates=False,
+    save_location="",
 ):
     """
     Generate n-grams from a text corpus and save the results in TSV files.
 
     Parameters:
-        corpus (str): The path to the input text file.
+        corpus (str): The path to the corpus file to be parsed.
         valid_chars (set, optional): A set of valid characters to consider for n-grams (default is alphanumeric and some special characters).
-        remove_chars (str, optional): Characters to remove from the text before processing (default is empty).
+        ordered (boolean, optional): Sort ngrams thus reducing the storage space.
+        removed (str, optional): Characters to remove from the text before processing (default is empty).
+        remove_duplicates (boolean, optional): Remove any bigrams that repeat a character twice in a row.
+        save_location (str, optional): Output directory, defaults to local directory.
     """
     if not valid_chars:
-        valid_chars = "abcdefghijklmnopqrstuvwxyz[];',./`1234567890-=\\"
+        valid_chars = "abcdefghijklmnopqrstuvwxyz[];',./`1234567890-=\\ "
     valid_chars = set(valid_chars)
 
     DATA_TYPES = {
-        # "characters": (1, 0),
+        "characters": (1, 0),
         "bigrams": (2, 0),
-        # "trigrams": (3, 0),
-        # "quadrigram": (4, 0),
+        "trigrams": (3, 0),
+        "quadrigram": (4, 0),
         "1-skip": (2, 1),
         # "2-skip": (2, 2),
         # "3-skip": (2, 3),
@@ -34,9 +39,7 @@ def get_grams(
         # "7-skip": (2, 7),
     }
 
-    unshift = str.maketrans(
-        '!@#$%^&*()_+:{}:<>|?"', "1234567890-=;[];,.\/'", remove_chars
-    )
+    unshift = str.maketrans('!@#$%^&*()_+:{}:<>|?"', "1234567890-=;[];,.\/'", removed)
     text = open(corpus, "r").read().lower().translate(unshift)
 
     def create(alias, size, skip):
@@ -51,12 +54,13 @@ def get_grams(
             if ordered:
                 gram = "".join(sorted(gram))
 
-            # only accept substrings with valid characters
-            if all(char in valid_chars for char in gram):
-                grams[gram] += 1
+            # only accept substrings with valid characters and filter duplicates
+            if not (remove_duplicates and size == 2 and gram[0] == gram[1]):
+                if all(c in valid_chars for c in gram):
+                    grams[gram] += 1
 
         # Output a TSV file of this data subtype
-        with open(f"output/{alias}.txt", "w") as f:
+        with open(save_location + f"/output/{alias}.txt", "w") as f:
             for chars, count in sorted(grams.items(), key=lambda x: -x[1]):
                 f.write(f"{chars}\t{count}\n")
 
--- run.py
+++ run.py
@@ -1,4 +1,39 @@
 from ngram_gen import get_grams
+import argparse
 
 if __name__ == "__main__":
-    get_grams("res/shai.txt", "abcdefghijklmnopqrstuvwxyz',.-", ordered=True)
+    parser = argparse.ArgumentParser(
+        description="Generate n-grams from a text corpus and save the results in TSV files."
+    )
+
+    parser.add_argument("corpus", help="The path to the corpus file to be parsed.")
+    parser.add_argument(
+        "save_location",
+        nargs="?",
+        help="Output directory, defaults to local directory if not specified.",
+        default=".",
+    )
+    parser.add_argument(
+        "-vc",
+        help="A set of valid characters to consider for n-grams (default is alphanumeric and some special characters).",
+        default="abcdefghijklmnopqrstuvwxyz',.-/;",
+    )
+    parser.add_argument(
+        "-rc",
+        help="Characters to remove from the text before processing (default is empty).",
+        default="",
+    )
+    parser.add_argument(
+        "-o",
+        help="(Boolean): sort ngrams thus reducing the storage space.",
+        default=False,
+    )
+    parser.add_argument(
+        "-rd",
+        help="(Boolean): remove any bigrams that repeat a character twice in a row.",
+        default=False,
+    )
+
+    args = parser.parse_args()
+
+    get_grams(args.corpus, args.vc, args.rc, args.o, args.rd, args.save_location)
